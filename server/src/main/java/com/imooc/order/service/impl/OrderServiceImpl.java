6af4f4e7dc244cb99791a4a65fc2c1adf5e60e26
xiaolei hu <huxiaolei1997@gmail.com>
2018-10-08 21:10:50 +0800

    整合redis

--- server/src/main/java/com/imooc/order/service/impl/OrderServiceImpl.java
+++ server/src/main/java/com/imooc/order/service/impl/OrderServiceImpl.java
@@ -46,8 +46,15 @@ public class OrderServiceImpl implements OrderService {
                 .map(OrderDetail::getProductId)
                 .collect(Collectors.toList());
         List<ProductInfoOutput> productInfoList = productClient.listForOrder(productIdList);
-//
-//       //计算总价
+
+        // 读 redis
+        // 减库存并将新值重新设置进 redis
+
+        // 订单入库异常， 手动回滚 redis
+        // 在可能抛出异常的地方加上 try catch，把之前减掉的库存又重新加回去
+
+
+        //计算总价
         BigDecimal orderAmount = new BigDecimal(BigInteger.ZERO);
 
         for (OrderDetail orderDetail : orderDTO.getOrderDetailList()) {
